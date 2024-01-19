e367d82fd419fbfcf45bd7ae4f8ab3d1e1f2924e
Tobias Kamm <tobias.kamm@tum.de>
2024-01-19 17:55:40 +0100

    Pointer finding works. Chains, caching and other options are still missing
    Co-authored-by: Jerg Kappeler <jerg.kappeler@tum.de>

--- ptrfind.py
+++ ptrfind.py
@@ -6,6 +6,7 @@ except ImportError:
 
 import argparse
 from types import SimpleNamespace
+import copy
 
 class PtrFind (gdb.Command):
   def __init__ (self):
@@ -37,8 +38,7 @@ class PtrFind (gdb.Command):
       print("[-] Failed to parse destination range")
       return
     
-    print(f"start: {hex(destination[0])}; end: {hex(destination[1])}")
-    
+        
     # Step 3: parse from
     start = None
     if args.start_region is not None:
@@ -47,20 +47,93 @@ class PtrFind (gdb.Command):
       except SyntaxError:
         print("[-] Failed to parse from-range")
         return
-      print(f"start: {hex(start[0])}; end: {hex(start[1])}")
-    else:
-      print("No from provided")
     
 
     # Step 4: parse mode
     if args.chain:
        print("Leak-chains active")
     elif args.leaks:
-       print("Leaks in a section")
+      PtrFind.find_pointers(destination, proc_mapping)
+    elif start is None: # from anywhere to ...
+      PtrFind.find_pointers(proc_mapping, destination)
+    else: # from ... to ...
+      PtrFind.find_pointers(start, destination)
+
+
+  def pretty_print_region(region, addr):
+    '''Returns the name of the region. If the address in that region has a debug symbol, attach the name to it'''
+    symbol = gdb.execute(f"info symbol {hex(addr)}", to_string=True)
+    if symbol.startswith("No symbol"):
+      return region.name
     else:
-       print("Normal mode")
+      # We have a symbol
+      symbol = symbol.split(" ", 3) 
+      return region.name + f" ({symbol[0]}{('+' + symbol[2]) if symbol[1] == '+' else ''})"
+
+
+  def find_pointers(search_range, proc_mapping):
+    '''Iterates over the serach range to find pointers to specific regions specified in the proc_mapping array'''
+    for region in search_range:
+      for addr in range(region.start, region.end, 8):
+          try:
+            val = PtrFind.deref(addr)
+          except gdb.MemoryError:
+            print(f"[-] Unable to access value at {hex(addr)}")
+          
+          region = PtrFind.get_region(proc_mapping, val)
+          if region is not None:
+            print(f"[+] Pointer to {PtrFind.pretty_print_region(region, addr)} found at {hex(addr)}")
+
+
+  def get_region(proc_mapping, addr, binary_search=True):
+    '''Returns the region that this address belongs to. Returns None if it does not belong to any'''
+    if binary_search:
+      start_index = 0
+      end_index = len(proc_mapping) - 1
+      while True:
+        if start_index == end_index or start_index + 1 == end_index:
+          break
+        # addr cannot be in that range
+        if addr < proc_mapping[start_index].start or addr >= proc_mapping[end_index].end:
+          return None
+        else:
+          # Take the middle. If it is an even number, take the righter objfile
+          middle_index = start_index + end_index >> 1
+          if(addr >= proc_mapping[middle_index].start):
+            start_index = middle_index
+          else:
+            end_index = middle_index
+          continue
+      
+      # Only two items left, does the second item match?
+      if start_index != end_index and addr >= proc_mapping[end_index].start and addr < proc_mapping[end_index].end:
+        return proc_mapping[end_index]
+
+      # Only one item left, so it must match
+      if addr >= proc_mapping[start_index].start and addr < proc_mapping[start_index].end:
+        return proc_mapping[start_index]
+      else:
+        return None
+    else:  
+      # Just iterate over the proc_mapping
+      if addr >= proc_mapping[0].start and addr < proc_mapping[len(proc_mapping)-1].end:
+        for m in proc_mapping:
+          if addr < m.end and addr >= m.start:
+                return m
+      return None
+
+
+
+  def deref(addr):
+    '''Returns the value at the provided address, or throws a gdb.MemoryError if the address is invalid'''
+    if type(addr) is not gdb.Value:
+      addr = gdb.Value(addr)
+    # I don't know why - but the gdb.MemoryError is only thrown when the resulting value is actually used
+    # So, we add 0 to it. This causes the gdb.MemoryError to be thrown in here
+    return addr.cast(addr.type.pointer()).referenced_value().const_value() + 0
 
   def parse_addr_region(proc_mapping, destination):     
+    '''Receives a user-provided region string and returns a subset of the proc_mapping that represents the search region'''
     destination_start = 0
     destination_end = 0
     if destination in ["heap", "stack", "libc", "image"]:      
@@ -69,11 +142,28 @@ class PtrFind (gdb.Command):
             or destination == "heap" and objfile.name == "[heap]" \
             or destination == "stack" and objfile.name == "[stack]" \
             or destination == "image" and objfile.name == gdb.current_progspace().filename:
-          destination_start = objfile.segments[0].start
-          destination_end = objfile.segments[len(objfile.segments) - 1].end
-          return (destination_start, destination_end)
+          return [objfile]
       print("[-] Failed to find region, please use address ranges manually")
       raise SyntaxError()
+    elif destination == "tls":
+      frame = gdb.newest_frame()
+      if frame.architecture().name() != "i386:x86-64":
+        print(f"[-] TLS is currently only supported on x86-64 (found {frame.architecture().name()}), please use manual address ranges")
+        raise SyntaxError()
+      
+      fs_base = frame.read_register("fs_base").const_value()
+      try:
+        val = PtrFind.deref(fs_base)
+      except gdb.MemoryError:
+        print("[-] Failed to find TLS. Reason: $fs_base points to an invalid address. Please use manual address ranges")
+        raise SyntaxError
+
+      if val != fs_base:
+        print("[!] TLS parsing might have failed, proceed with caution. Reason: Start of TLS does not contain a self-reference")
+      
+      tls = PtrFind.get_region(proc_mapping, fs_base)
+      tls.name = "[tls]"
+      return [tls]
     elif destination.count('-') == 1: # start-end
       destination = destination.split("-")
       destination_start = int(destination[0], 0)
@@ -87,26 +177,47 @@ class PtrFind (gdb.Command):
       # e.g. "/usr/lib64/ld-linux-x86-64.so.2" and "ld-linux-x86-64.so.2" will both work.
       for objfile in proc_mapping:
         if destination == objfile.name or ('/' in objfile.name and destination == objfile.name.rsplit('/', 1)[1]):
-          destination_start = objfile.segments[0].start
-          destination_end = objfile.segments[len(objfile.segments) - 1].end
-          return (destination_start, destination_end)
+          return [objfile]
         
       # Well, tough luck I guess
       raise SyntaxError()
-    
-    return (destination_start, destination_end)
 
+    # We land here if we provided an address range
+
+    def in_range(region): # if the region is fully inside the range, starting inside the range, or ending inside the range. Also, the range might be inside the region
+      return (destination_start <= region.start and destination_end >= region.end) or \
+      (destination_end > region.start and destination_end < region.end) or \
+      (destination_start >= region.start and destination_start <= region.end) or \
+      (destination_start >= region.start and destination_end <= region.end)
+      
+
+    destination_mapping = list(filter(in_range, copy.deepcopy(proc_mapping)))
+
+    if(len(destination_mapping) == 0):
+      print("[-] Provided address range is unmapped")
+      raise SyntaxError
+ 
+    for objfile in destination_mapping:
+      objfile.name = "user-defined region in " + objfile.name
+      objfile.segments = list(filter(in_range, objfile.segments))
+      objfile.segments[0].start = destination_start
+      objfile.segments[len(objfile.segments)-1] = destination_end
+    
+    destination_mapping[0].start = destination_start
+    destination_mapping[len(destination_mapping )-1].end = destination_end
+    return destination_mapping 
+    
 
   '''
-    Manually parse the output of `i proc m` into something that we can understand
+  Manually parse the output of `i proc m` into something that we can understand
 
-    we will return a list of strings containing objfiles
-    an objfile has a name and a list of segments
-    a segement has a start end size offset and permissions
-    permissions have a truth values for read, write andexecute
-    
-    gdb builtin get objfiles/ get spaces doesn't return what we expect
-    '''
+  we will return a list of strings containing objfiles
+  an objfile has a name and a list of segments
+  a segement has a start end size offset and permissions
+  permissions have a truth values for read, write andexecute
+  
+  gdb builtin get objfiles/ get spaces doesn't return what we expect
+  '''
   def create_proc_map():
     '''Manually parse the output of `i proc m` into something that we can understand'''
     mappings_output = gdb.execute("info proc mappings", to_string=True).splitlines()[4:]
@@ -115,12 +226,12 @@ class PtrFind (gdb.Command):
     current_objfile = None
     
     for line in mappings_output:
-      line_entries = list(filter(lambda x: x != '' , line.split(" ")))
+      line_entries = list(filter(lambda x: x != '' and x != '\t' , line.split(" ")))
 
       segment = SimpleNamespace(
         start = int(line_entries[0],16),
         end = int(line_entries[1],16),
-        size = int(line_entries[2],16),
+        #size = int(line_entries[2],16),
         offset = int(line_entries[3],16),
         perissions = PtrFind.parse_page_permissions(line_entries[4])
       )
@@ -130,12 +241,20 @@ class PtrFind (gdb.Command):
         if current_objfile is not None:
           objfiles.append(current_objfile)
         
-        new_name = ""
-        if len(line_entries)==6:
+        new_name = ''
+        if len(line_entries) == 6:
           new_name = line_entries[5]
 
-        current_objfile = SimpleNamespace(name=new_name,segments =[] )
+        current_objfile = SimpleNamespace(
+          name=new_name,
+          segments=[],
+          start = segment.start,
+          end = segment.end
+        )
       
+      if current_objfile.name == "":
+        current_objfile.name = f"[{hex(current_objfile.start)}-{hex(current_objfile.end)}]"
+      current_objfile.end = segment.end
       current_objfile.segments.append(segment)
     return objfiles
     
